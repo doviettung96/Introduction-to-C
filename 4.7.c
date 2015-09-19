f11971334a50161d919bf7ae9d0b401c722404a9
doviettung96 <tunga9td@gmail.com>
2015-09-19 19:04:57 +0700

    thanh toan tien sach

--- 4.7.c
+++ 4.7.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+#define YELLOW "\x1b[43m"
+#define RESET "\x1b[0m"
 main ()
 {
   char frame[] = "________________________________________________";
-  int dodai;
   int Quantity;
   float Price, Total, VAT, Pay;
   char ISBN[7], Title[15]; 
@@ -10,19 +11,19 @@ main ()
   printf("ISBN: ?\n"); scanf("%7s",ISBN);
   printf("Title: ?\n"); scanf(" %[^\n]",Title);
   printf("Price= ?\n"); scanf("%8f",&Price);
-  VAT = (4 /(double) 100);
+  VAT = (4 /(float) 100);
   Total = Price*Quantity;
   Pay = Total*(1 + VAT);
-  printf("%s\n",frame);
-  printf("%-3c%-44s|\n",'|',"BK Bookseller");
-  printf("|%47c\n",'|');
-  printf("|%47c\n",'|');
-  printf("%-3c%-6s%-7s%-15s%-8s%-8s|\n",'|',"Qty","ISBN","Title","Price","Total");
-  printf("%-3c%-6d%-7s%-15s%-8g%-8g|\n",'|',Quantity,ISBN,Title,Price,Total);
-  printf("|%-.43s%4c\n",frame,'|');
-  printf("|%47c\n",'|');
-  printf("|%47c\n",'|');
-  printf("%-3c%-44s|\n",'|',"VAT: 4%");
-  printf("%-3c%-8s%-8g%28c\n",'|',"You pay:",Pay,'|');
-  printf("|%-.46s|\n",frame);
+  printf("\n%s\n",frame);
+  printf(""YELLOW"%-3c%-44s|"RESET"\n",'|',"BK Bookseller");
+  printf(""YELLOW"|%47c"RESET"\n",'|');
+  printf(""YELLOW"|%47c"RESET"\n",'|');
+  printf(""YELLOW"%-3c%-6s%-7s%-15s%-8s%-8s|"RESET"\n",'|',"Qty","ISBN","Title","Price","Total");
+  printf(""YELLOW"%-3c%-6d%-7s%-15s%-8g%-8g|"RESET"\n",'|',Quantity,ISBN,Title,Price,Total);
+  printf(""YELLOW"|%-.43s%4c"RESET"\n",frame,'|');
+  printf(""YELLOW"|%47c"RESET"\n",'|');
+  printf(""YELLOW"|%47c"RESET"\n",'|');
+  printf(""YELLOW"%-3c%-4s %-39g|"RESET"\n",'|',"VAT:",VAT);
+  printf(""YELLOW"%-3c%-8s %-8g%28c"RESET"\n",'|',"You pay:",Pay,'|');
+  printf(""YELLOW"|%-.46s|"RESET"\n",frame);
 }
