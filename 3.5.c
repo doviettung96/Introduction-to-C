41c86dbf5cbcc39663206af2018c3fd470a26c7a
doviettung96 <tunga9td@gmail.com>
2015-09-19 21:14:20 +0700

    Renting value

---
+++ 3.5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+main ()
+{
+  char day[9];
+  int overnight,threeday,weekly,total;
+  printf("Welcome to BK library DVD shop\n");
+  printf("The day of the week: "); scanf("%s",day); printf("%s\n",day);
+  printf("The number of overnight: "); scanf("%d",&overnight); printf("%d\n",overnight);
+  printf("The number of three-day: "); scanf("%d",&threeday); printf("%d\n",threeday);
+  printf("The number of weekly: "); scanf("%d",&weekly); printf("%d\n",weekly);
+  total = 7*overnight + 5*threeday + 3*weekly;
+  printf("Total cost= %d$\n",total);
+}
+  
+  
