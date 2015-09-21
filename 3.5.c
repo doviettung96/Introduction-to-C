fe0d972e11acb569f5f3e3a9369793910302d91f
doviettung96 <tunga9td@gmail.com>
2015-09-21 21:14:41 +0700

    Renting DVD cost

--- 3.5.c
+++ 3.5.c
@@ -8,7 +8,7 @@ main ()
   printf("The number of overnight: "); scanf("%d",&overnight); printf("%d\n",overnight);
   printf("The number of three-day: "); scanf("%d",&threeday); printf("%d\n",threeday);
   printf("The number of weekly: "); scanf("%d",&weekly); printf("%d\n",weekly);
-  total = 7*overnight + 5*threeday + 3*weekly;
+  total = 7*overnight + 15*threeday + 21*weekly;
   printf("Total cost= %d$\n",total);
 }
   
