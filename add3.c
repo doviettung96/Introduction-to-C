22e634570c9d82f7849079480d41d830cc3e4a49
doviettung96 <tunga9td@gmail.com>
2015-09-22 20:47:31 +0700

    conversion

--- add3.c
+++ add3.c
@@ -5,8 +5,8 @@ main ()
   printf("Input two temperatures:\n");
   printf("C1= "); scanf("%f",&C1);
   printf("F2= "); scanf("%f",&F2);
-  F1 = 9*C1/(float)5 + 32;
-  C2 = 5*(F2-32)/(float)9;
-  printf("The converted value of C1 in F is %g Fahrenheit degree\n",F1);
-  printf("The converted value of F2 in C is %g Celsius degree\n",C2);
+  F1 = 9.0*C1/5.0 + 32;
+  C2 = 5.0*(F2-32)/9.0;
+  printf("The converted value of C1 in F is %g Fahrenheitdegree\n",F1);
+  printf("The converted value of F2 in C is %g Celsiusdegree\n",C2);
 }
