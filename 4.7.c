ae72f13f4f427267a07c0f20e3b27a34d83c93e4
doviettung96 <tunga9td@gmail.com>
2015-09-22 20:45:24 +0700

    bookstore

--- 4.7.c
+++ 4.7.c
@@ -3,10 +3,10 @@
 #define RESET "\x1b[0m"
 main ()
 {
-  char frame[] = "________________________________________________";
+  char frame[] = "______________________________________________________________";
   int Quantity;
   float Price, Total, VAT, Pay;
-  char ISBN[7], Title[15]; 
+  char ISBN[7], Title[20]; 
   printf("Quantity: ?\n"); scanf("%6d",&Quantity);
   printf("ISBN: ?\n"); scanf("%7s",ISBN);
   printf("Title: ?\n"); scanf(" %[^\n]",Title);
@@ -15,15 +15,15 @@ main ()
   Total = Price*Quantity;
   Pay = Total*(1 + VAT);
   printf("\n%s\n",frame);
-  printf(""YELLOW"%-3c%-44s|"RESET"\n",'|',"BK Bookseller");
-  printf(""YELLOW"|%47c"RESET"\n",'|');
-  printf(""YELLOW"|%47c"RESET"\n",'|');
-  printf(""YELLOW"%-3c%-6s%-7s%-15s%-8s%-8s|"RESET"\n",'|',"Qty","ISBN","Title","Price","Total");
-  printf(""YELLOW"%-3c%-6d%-7s%-15s%-8g%-8g|"RESET"\n",'|',Quantity,ISBN,Title,Price,Total);
-  printf(""YELLOW"|%-.43s%4c"RESET"\n",frame,'|');
-  printf(""YELLOW"|%47c"RESET"\n",'|');
-  printf(""YELLOW"|%47c"RESET"\n",'|');
-  printf(""YELLOW"%-3c%-4s %-39g|"RESET"\n",'|',"VAT:",VAT);
-  printf(""YELLOW"%-3c%-8s %-8g%28c"RESET"\n",'|',"You pay:",Pay,'|');
-  printf(""YELLOW"|%-.46s|"RESET"\n",frame);
+  printf(""YELLOW"%-3c%-58s|"RESET"\n",'|',"BK Bookseller");
+  printf(""YELLOW"|%61c"RESET"\n",'|');
+  printf(""YELLOW"|%61c"RESET"\n",'|');
+  printf(""YELLOW"%-3c%-6s%-7s%-25s%-10s%-10s|"RESET"\n",'|',"Qty","ISBN","Title","Price","Total");
+  printf(""YELLOW"%-3c%-6d%-7s%-25s%-10g%-10g|"RESET"\n",'|',Quantity,ISBN,Title,Price,Total);
+  printf(""YELLOW"|%-.57s%4c"RESET"\n",frame,'|');
+  printf(""YELLOW"|%61c"RESET"\n",'|');
+  printf(""YELLOW"|%61c"RESET"\n",'|');
+  printf(""YELLOW"%-3c%-4s %-53g|"RESET"\n",'|',"VAT:",VAT);
+  printf(""YELLOW"%-3c%-8s %-8g%42c"RESET"\n",'|',"You pay:",Pay,'|');
+  printf(""YELLOW"|%-.60s|"RESET"\n",frame);
 }
