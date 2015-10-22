7b37fd46acb0eb5cd206f5fd476e8db4816861d7
doviettung96 <tunga9td@gmail.com>
2015-10-22 22:51:35 +0700

    tim so trau va co

--- week7/trau.c
+++ week7/trau.c
@@ -2,15 +2,15 @@
 main(){
   int x,y,z,i;
   i=0;
-  for(z=0;z<=100/3;z++){
-    for(x=20-z/5;x>=0;x--){
-      y=100-x-3*z;
-      if(5*x+3*y+z==100){
+  for(x=0;x<=20;x++){
+    for(z=100-x;z>=0;z--){
+      y=100-x-z;
+      if((5*x+3*y+z/3==100)&&(z%3==0)){
         i++;
   printf("Truong hop %d:\n",i);
   printf("Co %d trau dung, an %d bo co.\n",x,5*x);
   printf("Co %d trau nam, an %d bo co.\n",y,3*y);
-  printf("Co %d trau gia, an %d bo co.\n",3*z,z);
+  printf("Co %d trau gia, an %d bo co.\n",z,z/3);
   }
     }
   }
