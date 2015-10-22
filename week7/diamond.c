79e9174910257b4c468aa301490d2a15acdfbb8f
doviettung96 <tunga9td@gmail.com>
2015-10-22 21:38:19 +0700

    Hinh thoi

--- week7/diamond.c
+++ week7/diamond.c
@@ -1,25 +1,19 @@
 #include <stdio.h>
 main()
 {
-  int x,y,a,b,c,d,n;
-  printf("Type n: \n"); scanf("%d",&n); printf("Height is: %d\n",2*n-1);
-  a=n; b=a; c=a; d=a;
+  int x,y,a,b,n;
+  printf("Height: \n"); scanf("%d",&n);
+  a=b=n;
   for (y=1;y<=n;y++){
-    for(x=1;x<=n;x++)
-      printf("%c",x<a?' ':'*');
-      a--;
-    for(x=n+1;x<=2*n-1;x++)
-	       printf("%c",x>b?' ':'*');
-	       b++;
+    for(x=1;x<=2*n-1;x++)
+      printf("%c",(x<a||x>b)?' ':'*');
+    a--; b++; printf("\n");
+  }
+  for (y=n+1;y<=2*n-1;y++){
+    for(x=1;x<=2*n-1;x++)
+      printf("%c",(x<a+2||x>b-2)?' ':'*');
+    a++; b--;
     printf("\n");
   }
-  for(y=n+1;y<=2*n-1;y++){
-      for(x=n;x>=1;x--)
-	printf("%c",x>c-1?' ':'*');
-      c--;
-      for(x=2*n-1;x>=n+1;x--)
-	printf("%c",x<d+2?' ':'*');
-      d++;
-      printf("\n");
-  }
 }
+    
