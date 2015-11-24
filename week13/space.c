71077a09d59c3f3e152dcf3c9af6954c5d58cf3e
doviettung96 <tunga9td@gmail.com>
2015-11-24 21:39:47 +0700

    sp

--- week13/space.c
+++ week13/space.c
@@ -5,49 +5,52 @@
 #define size 15
 
 void trimleft(char *a);
-char trimright(char *a);
-char trimiddle(char *a);
+void trimright(char *a);
+void trimiddle(char *a);
 
 int main()
 {
-  char a[n + 1];
+  char a[size + 1];
   printf("Type in the string\n");
   gets(a);
   trimleft(a);
+  printf("%-16s","Remove left ");
+  printf("%-16s\n",a);
   trimright(a);
+  printf("%-16s","Remove right ");
+  printf("%-16s\n",a);
   trimiddle(a);
+  printf("%-16s","Remove middle ");
+  printf("%-16s\n",a);
   return 0;
 }
 
 void trimleft(char *a)
 {
-  int i,j;
+  int i;
   while(*a == ' ')
-    for (i = 0; i < strlen(a); ++i){
+    for (i = 0; i < strlen(a); ++i)
       *(a + i) = *(a + i + 1);
-    }
 }
 
-int trimright(char *a)
-{
-  int i,j;
-  int right = 0;
-  for (i = size -1 ; i >= 0; --i){
-    if(!isspace(*(a + i)))
-      break;
-    right++;
-  }
-  return right;
-}
-
-void trimiddle(char *a, int size, int left, int right)
+void trimright(char *a)
 {
   int i;
-  for(i = left; i < size - right; ++i){
-    if(isspace(*(a + i)) && isspace(*(a + i + 1))){
-      *(a + i) = '\0';
-    }
-  }
+  while(*(a + strlen(a)) == ' ')
+    *(a + strlen(a)) = *(a + strlen(a) + 1); 
 }
 
-void out(char *a)
+void trimiddle(char *a)
+{
+  int i,j;
+  int n = strlen(a);
+  for(i = 0; i < n; ++i){
+    if(*(a + i) == ' ' && *(a + i + 1) == ' ')
+      {
+        for(j = i; j < n; ++j)
+          *(a + j) = *(a + j + 1);
+        n--;
+        i--;
+      }
+  }
+}
